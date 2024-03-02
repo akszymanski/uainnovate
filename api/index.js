d37bdf6fa9536f961895760e1562c0d6d63a0e0d
Supergeek57 <copperstem@gmail.com>
2024-03-02 17:40:20 -0600

    Backend: Add student and Add Student HR

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 var Express = require("express");
 var MongoClient = require("mongodb").MongoClient;
 var cors = require("cors");
+var multer = require("multer");
 
 var app = Express();
 app.use(cors());
+app.use(Express.json());
+
 
 var CONNECTION_URL = "mongodb+srv://uainnovate:qhHSn7lPYrcSRFvN@cluster0.fdvzwdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 var DATABASE_NAME = "uainnovatedb";
@@ -44,17 +47,73 @@ app.get('/api/GetData', (request, response) => {
     });
 });
 
-app.post('/api/AddStudent', (formData, response) => {
-    database.collection("uainnovatecollection").insertOne(formData.body, (error, result) => {
-        if (error) {
-            console.error('Error occurred while adding data to MongoDB Atlas...\n', error);
-            response.status(500).send('Internal Server Error');
-            return;
-        }
+//app.post('/api/AddStudent', (formData, response) => {
+//    database.collection("uainnovatecollection").insert(formData.body, (error, result) => {
+//        if (error) {
+//            console.error('Error occurred while adding data to MongoDB Atlas...\n', error);
+//            response.status(500).send('Internal Server Error');
+//            return;
+//        }
+//
+//        response.send(result);
+//    });
+//});
 
-        response.send(result);
+app.post('/api/AddStudentHR', multer().none(), (request, response) => {
+    console.log("In post");
+    console.log(request.body);
+    //const obj = JSON.parse(request.body);
+    const collection = database.collection("uainnovatecollection");
+    const newStudent = {
+        _id: request.body.email,
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        phone: request.body.phone,
+        officeLocations: request.body.officeLocations,
+        linkedin: request.body.linkedin,
+        graduationDate: request.body.graduationDate,
+        school: request.body.school,
+        interviewStage: request.body.interviewStage,
+        interviewFeedback: request.body.interviewFeedback,
+        evaluationMetric: request.body.evaluationMetric
+
+    };
+    collection.insertOne(newStudent, (error, result) => {
+        if (error) {
+            console.error('Error inserting document: ', error);
+            response.status(500).send(error);
+        } else {
+            response.send(result);
+        }
     });
 });
+
+app.post('/api/AddStudent', multer().none(), (request, response) => {
+    console.log("In post");
+    console.log(request.body);
+    const collection = database.collection("uainnovatecollection");
+    const newStudent = {
+        _id: request.body.email,
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        phone: request.body.phone,
+        officeLocations: request.body.officeLocations,
+        linkedin: request.body.linkedin,
+        graduationDate: request.body.graduationDate,
+        school: request.body.school
+
+    };
+    collection.insertOne(newStudent, (error, result) => {
+        if (error) {
+            console.error('Error inserting document: ', error);
+            response.status(500).send(error);
+        } else {
+            response.send(result);
+        }
+    });
+});
+
+
 
 // Close the MongoDB client when the application terminates
 process.on('exit', (code) => {
